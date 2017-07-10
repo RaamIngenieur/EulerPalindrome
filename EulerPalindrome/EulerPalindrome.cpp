091f7bb7395a85373c975056cb84ff9e7cbe1b26
Raam <shriraam.mohan@gmail.com>
2017-07-10 17:37:24 +0200

    Palindrome check

--- EulerPalindrome/EulerPalindrome.cpp
+++ EulerPalindrome/EulerPalindrome.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
 
 int main()
 {
+	int i, j,palPro=0,pro;
+	string palTest,palTest1;
+
+	for (i = 100; i < 1000; i++)
+	{
+		for (j = 100; j < 1000; j++)
+		{
+			pro = i*j;
+			palTest = to_string(pro);
+			palTest1 = palTest;
+			reverse(palTest.begin(), palTest.end());
+			(palTest1 == palTest) ? (pro > palPro ? palPro = pro : pro) : pro;
+		}
+	}
+
+	cout << "Highest palindrome is "<< palPro << endl;
+	cin.get();
+
+
     return 0;
 }
 
